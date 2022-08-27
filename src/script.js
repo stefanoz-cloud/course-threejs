979a38daa8c1a3bfa12c9531995bedddb37caabe
Zogno Stefano <ZognoS@corvallis.it>
2022-08-27 17:36:12 +0200

    template

---
+++ src/script.js
@@ -0,0 +1,64 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+camera.position.z = 5;
+scene.add(camera);
+
+// Object
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
+// Mesh Object And Add In Scene
+const object = new THREE.Mesh(geometry,material);
+scene.add(object);
+
+// Resize
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animate
+const animate = () =>
+{
+    // Update objects
+    object.rotation.x += 0.01;
+	object.rotation.y += 0.01;
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(animate);
+}
+
+animate();
